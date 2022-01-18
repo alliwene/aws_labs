1bc1c1ead22717aae9801aaa66f6788a46f3d5e4
Abiodun Allison <allisonabiodun@gmail.com>
2022-01-18 10:45:09 +0100

    cleanup

---
+++ reStart/py_scripts/delete-bucket-versions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import boto3
+
+BUCKET = 'aua-test-09898'
+session = boto3.Session(profile_name='admin')
+s3 = session.resource('s3')
+bucket = s3.Bucket(BUCKET)
+bucket.object_versions.delete()
+
+# if you want to delete the now-empty bucket as well, uncomment this line:
+bucket.delete() 
